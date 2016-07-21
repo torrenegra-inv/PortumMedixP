e1709e73658a8034e40e09a5a8e8d87e190dd42e
Jorge Torreblanca <torrenegra.inv@gmail.com>
2016-07-21 10:37:34 -0500

    Agregar archivos de proyecto.

---
+++ PortumMedixP/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PortumMedixP.Startup))]
+namespace PortumMedixP
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
